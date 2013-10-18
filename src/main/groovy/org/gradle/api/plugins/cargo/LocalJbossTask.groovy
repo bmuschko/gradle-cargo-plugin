8156076f89a2bd3e7440043b8676b6d9776c2816
voitau <aliaksandr.voitau@viacomcontractor.com>
2013-10-18 16:17:14 -0400

    Added "cargo.jboss.jrmp.invoker.port" property handling.

--- src/main/groovy/org/gradle/api/plugins/cargo/LocalJbossTask.groovy
+++ src/main/groovy/org/gradle/api/plugins/cargo/LocalJbossTask.groovy
@@ -11,6 +11,7 @@ import org.gradle.api.tasks.Optional
  */
 class LocalJbossTask extends LocalContainerTask {
     @Input @Optional Integer invokerPoolPort
+    @Input @Optional Integer jrmpInvokerPort
     @Input @Optional Integer namingPort
     @Input @Optional String configuration
 
@@ -18,6 +19,9 @@ class LocalJbossTask extends LocalContainerTask {
     void setContainerSpecificProperties() {
         if(getInvokerPoolPort()) {
             ant.property(name: LocalJbossTaskProperty.INVOKER_POOL_PORT.name, value: getInvokerPoolPort())
+        }
+        if(getJrmpInvokerPort()) {
+            ant.property(name: LocalJbossTaskProperty.JRMP_INVOKER_PORT.name, value: getJrmpInvokerPort())
         }
         if(getNamingPort()) {
             ant.property(name: LocalJbossTaskProperty.NAMING_PORT.name, value: getNamingPort())
